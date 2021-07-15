7afa9bbdd49d5bf9150a5a8314c292137f3da569
victor <akpomudje@gmail.com>
2021-07-15 22:25:00 +0100

    fix

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -90,7 +90,7 @@
             slidesToShow: 3,
             slidesToScroll: 2,
             autoplay: true,
-            centerMode: true,
+            // centerMode: true,
             variableWidth: true,
             centerPadding: '60px',
             infinite: true,
